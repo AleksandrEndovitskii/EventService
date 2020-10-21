0b938a10ff5ebf385a5b6f5e3eae94b68fe461cc
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-10-22 01:16:49 +0300

    Add empty implementation of EventService

--- Assets/Scripts/EventService.cs
+++ Assets/Scripts/EventService.cs
@@ -1,17 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EventService : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void TrackEvent(string type, string data)
     {
         
     }
