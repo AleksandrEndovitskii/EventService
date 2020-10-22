1114165befd6d4e8b6c2b7ad3839e957b612160d
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-10-23 01:07:19 +0300

    Add custom Post method for sending raw json

--- Assets/Scripts/EventService.cs
+++ Assets/Scripts/EventService.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class EventService : MonoBehaviour
 {
@@ -56,5 +58,19 @@ public class EventService : MonoBehaviour
 
             _trackableEventsJsonObject = new TrackableEventsJsonObject();
         }
+    }
+
+    //https://forum.unity.com/threads/posting-json-through-unitywebrequest.476254/
+    IEnumerator Post(string url, string bodyJsonString)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
     }
 }
