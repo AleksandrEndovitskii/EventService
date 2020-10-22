99ca4a67b54f6a15c311bd6cd199d7db72417c9e
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-10-22 03:00:17 +0300

    Add demo implementation of EventServiceTester

--- Assets/Scripts/EventServiceTester.cs
+++ Assets/Scripts/EventServiceTester.cs
@@ -1,6 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class EventServiceTester : MonoBehaviour
 {
-    
+    [SerializeField]
+    private EventService _eventService;
+    [SerializeField]
+    private int _testTrackEventsCount = 10;
+    [SerializeField]
+    private int _cooldownBeforeTrackEventSecondsCount = 1;
+
+    private Coroutine _testTrackEventCoroutine;
+
+    private void Start()
+    {
+        Initialize();
+    }
+    private void OnDestroy()
+    {
+        UnInitialize();
+    }
+
+    private void Initialize()
+    {
+        _testTrackEventCoroutine = StartCoroutine(TestTrackEventCoroutine());
+    }
+    private void UnInitialize()
+    {
+        StopCoroutine(_testTrackEventCoroutine);
+    }
+
+    private IEnumerator TestTrackEventCoroutine()
+    {
+        for (var i = 0; i < _testTrackEventsCount; i++)
+        {
+            yield return new WaitForSeconds(_cooldownBeforeTrackEventSecondsCount);
+
+            Debug.Log("TrackEvent");
+        }
+    }
 }
