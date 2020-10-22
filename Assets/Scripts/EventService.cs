e25b47896e5f1ee7272f9df5d62adc1cfed6173a
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-10-23 00:09:49 +0300

    Decrease cooldown before send in event service

--- Assets/Scripts/EventService.cs
+++ Assets/Scripts/EventService.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class EventService : MonoBehaviour
 {
     [SerializeField]
-    private int _cooldownBeforeSend = 10;
+    private int _cooldownBeforeSend = 3;
 
     private TrackableEventsJsonObject _trackableEventsJsonObject;
 
